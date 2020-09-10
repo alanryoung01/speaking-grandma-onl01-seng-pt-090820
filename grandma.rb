366f9036f99decf40f9e0dd9dc3ff4d827e912f5
Alan Young <alanryoung01@gmail.com>
2020-09-10 20:13:51 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,10 +14,10 @@
 
 def speak_to_grandma(speak)
   if speak == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif speak == speak.downcase
-    puts "HUH! SPEAK UP, SONNY!"
+    return "HUH! SPEAK UP, SONNY!"
   elsif speak == speak.upcase
-    puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   end
 end
